de912d4a6cde0aabe0d5c2f34a9e9fe49ea89bb0
Konstantin <kopamedve@gmail.com>
2024-10-21 19:04:43 +0200

    Rename output file with timestamp in run script: update output path to include date and time

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,8 +9,11 @@ cp sketch_3d_world_render.pde sketch_folder/sketch_folder.pde
 # Run the processing-java command
 processing-java --sketch=$(pwd)/sketch_folder --output=$(pwd)/output --force --run
 
-# Copy the generated output file to the project root
-cp sketch_folder/output/output.tif $(pwd)/images/output.tif
+# Get the current date and time in a safe format for file names
+datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Copy the generated output file to the project root and rename it
+cp sketch_folder/output/output.tif $(pwd)/images/world_$datetime.tif
 
 # Cleanup: remove the temporary sketch_folder and output folder
 rm -rf sketch_folder output
